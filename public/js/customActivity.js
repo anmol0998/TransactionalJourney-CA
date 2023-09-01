db1f9354bdf52503cbd2826b726f795f0451f635
anmol0998 <104828995+anmol0998@users.noreply.github.com>
2023-09-01 14:59:30 +0530

    updtaed

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,6 +1,6 @@
 define([
     'postmonger'
-], function(
+], function (
     Postmonger
 ) {
     'use strict';
@@ -18,31 +18,37 @@ define([
     connection.on('initActivity', initialize);
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
-
     connection.on('clickedNext', save);
-
+//////////////////////////////////////////////////////////////////////////
+    var eventDefinitionKey;
+    connection.trigger('requestInteraction');
+    connection.on('requestedInteraction', function (settings) {
+        eventDefinitionKey = settings;
+        console.log('settings'+settings);
+    });
+/////////////////////////////////////////////////////////////////////////////
     function onRender() {
         connection.trigger('ready');
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
     }
 
-  function initialize(data) {
-    console.log("Initializing data data: "+ JSON.stringify(data));
+    function initialize(data) {
+        console.log("Initializing data data: " + JSON.stringify(data));
         if (data) {
             payload = data;
-        }    
+        }
 
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
             payload['arguments'].execute.inArguments &&
             payload['arguments'].execute.inArguments.length > 0
-         );
+        );
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-        
-        console.log('Has In arguments: '+JSON.stringify(inArguments));
+
+        console.log('Has In arguments: ' + JSON.stringify(inArguments));
 
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
@@ -61,7 +67,7 @@ define([
 
                 if (key === 'body') {
                     $('#messageBody').val(val);
-                }                                                               
+                }
 
             })
         });
@@ -74,13 +80,13 @@ define([
 
     }
 
-    function onGetTokens (tokens) {
-        console.log("Tokens function: "+JSON.stringify(tokens));
+    function onGetTokens(tokens) {
+        console.log("Tokens function: " + JSON.stringify(tokens));
     }
 
-    function onGetEndpoints (endpoints) {
-        console.log("Get End Points function: "+JSON.stringify(endpoints));
-    } 
+    function onGetEndpoints(endpoints) {
+        console.log("Get End Points function: " + JSON.stringify(endpoints));
+    }
 
     function save() {
 
@@ -93,15 +99,15 @@ define([
         }];
 
         payload['metaData'].isConfigured = true;
-        console.log("Payload on SAVE function: "+JSON.stringify(payload));
+        console.log("Payload on SAVE function: " + JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
         /////////////////////////////////////////////
-        postmonger.on('requestedInteraction', function(interaction) {
+        postmonger.on('requestedInteraction', function (interaction) {
             // Handle the interaction data received from Journey Builder
             console.log('Interaction Data:', interaction);
             console.log('Interaction JSON Data:', JSON.stringify(interaction));
             // You can access the interaction data properties like interaction.id, interaction.name, etc.
-          });          
+        });
 
-    }                    
+    }
 });
